1943165bfd13c1995956f1f1994fc1c12d8d07dc
damcb164e21 <julio61907@gmail.com>
2015-05-28 19:07:58 -0500

    2subida

--- jsapli1/acciones.js
+++ jsapli1/acciones.js
@@ -2,6 +2,14 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
-}); 
-});
 
+  $('#beep').tap(function(){
+  navigator.notification.beep(1);
+  });//tap´beep
+
+  $('#vibrar').tap(function(){
+  navigator.notification.vibrate(1000);
+  });//tap vibrar
+
+  },false); //deviceready
+});//ready
