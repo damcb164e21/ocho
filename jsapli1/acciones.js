6f2fed4c0751f2af8a40194cd4bbf877ee250460
damcb164e21 <julio61907@gmail.com>
2015-05-28 19:21:19 -0500

    3subida

--- jsapli1/acciones.js
+++ jsapli1/acciones.js
@@ -1,8 +1,13 @@
 // JavaScript Document
-$(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	
 
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
+		
   $('#beep').tap(function(){
   navigator.notification.beep(1);
   });//tap´beep
@@ -11,5 +16,45 @@ document.addEventListener("deviceready",function(){
   navigator.notification.vibrate(1000);
   });//tap vibrar
 
-  },false); //deviceready
-});//ready
+		$('#izquierda').on ("swipeleft", function(){
+			navigator.notification.alert("deslizó a la izquierda",function(){"Aplicacion5","Aceptar"});
+});//barrer izquierda}
+
+
+        $('#derecha').on("swiperight", function(){
+			navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+				switch(opt)
+				{
+					
+					case 1:
+					navigator.notification.beep(1);
+					break;
+					
+					case 2:
+					navigator.notification.vibrate(1000);
+					break;
+				}//switch opt
+			},"Aplicacion5","Beep,Vibrar,Cancelar");//confirm
+			
+		});//barrer derecha
+		document.addEventListener("pause",function(){
+			escribehistoria('la app se pauso');
+		},false);//pausa
+		document.addEventListener("resume",function(){
+			escribehistoria('la app se reinicio');
+		},false);//resume
+		document.addEventListener("onLine",function(){
+			escribehistoria('la app se conecto a la red');
+		},false);//se conecto
+		document.addEventListener("offLine",function(){
+			escribehistoria('la app se desconecto de la red');
+		},false);//se desconecto
+		
+	},false);//ready device
+	
+});//document
+
+function escribehistoria(accion){
+	$('#eHistoria').append('<li>'+accion+'</li>');
+}
+
